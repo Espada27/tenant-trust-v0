6f2148c956ed7fdd25fcf12cc047380a2ce72a53
espada27 <jonathandugard@gmail.com>
2023-11-30 11:46:28 +0100

    Update project config

--- backend/hardhat.config.js
+++ backend/hardhat.config.js
@@ -1,6 +1,33 @@
+require("hardhat-watcher");
 require("@nomicfoundation/hardhat-toolbox");
+require("hardhat-gas-reporter");
+require("solidity-coverage");
+require("@nomicfoundation/hardhat-verify");
 
-/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
-  solidity: "0.8.19",
+  defaultNetwork: "hardhat",
+  networks: {
+    localhost: {
+      url: "http://127.0.0.1:8545",
+      chainId: 31337,
+    },
+  },
+
+  gasReporter: {
+    enabled: true,
+  },
+
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.23",
+      },
+    ],
+  },
+  watcher: {
+    test: {
+      tasks: ["test"],
+      files: ["./test/**/*"],
+    },
+  },
 };
